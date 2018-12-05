b91330acef45317a2c589bd643dcaa73e1530795
MarioBurgos <mburgomaes@gmail.com>
2018-12-05 19:00:40 +0100

    Ejercicio 1 echo, calcular números primos

--- app/src/main/java/com/example/mario/practica4/MainActivity.java
+++ app/src/main/java/com/example/mario/practica4/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.mario.practica4;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn1 = findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent btn1 = new Intent(MainActivity.this, NumerosPrimos.class );
+                startActivity(btn1);
+            }
+        });
     }
 }
